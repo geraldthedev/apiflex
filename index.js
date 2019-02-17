6c717e752825400646e681d373f6fe4692d4bdac
Gerald Bryant <hakeem1106@gmail.com>
2019-02-17 21:59:03 +0000

    progress

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+
 let body = document.getElementById("root");
 let space = document.createElement("div");
 const pieces = document.createElement("input");
 const addbutto = document.createElement("button");
 
+body.appendChild(space).setAttribute("class", "container-fluid");
+body.appendChild(addbutto).setAttribute("id", "taken");
 
 
-couta = () => {
-body.appendChild(space);
-body.appendChild(addbutto);
-space.appendChild(pieces);
 
-addbutto.onclick();
-}
+addbutto.addEventListener("click", couta = ()=>{
+
+    body.appendChild(space);
+    body.appendChild(pieces);
+});
 
 
-couta(body,space,pieces,addbutto);
+
+
+
+
 
 
