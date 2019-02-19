c923adbb78c267b7bb15c24954c7f20ad3419bc5
Gerald Bryant <hakeem1106@gmail.com>
2019-02-19 20:18:43 +0000

    db connected

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-import mongoose from 'mongoose';
 const express = require('express');
 const app = express();
 const port = 8080;
 const bodyParser = require('body-parser');
 
-db = mongoose.connect("mongodb+srv://hakeem1106:Megan1106@testclust-w12h6.mongodb.net/test?retryWrites=true");
-
-let nameSave = new mongoose.Schema({
-    name: String
-});
 
 
 
